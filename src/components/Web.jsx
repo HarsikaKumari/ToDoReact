a62c67ca50bd455e7a0eeb697edb4fe522128a23
Harsika Kumari <kumariharsika8@gmail.com>
2024-04-12 00:02:21 +0530

    feat(add item): managed to add item in the list

--- src/components/Web.jsx
+++ src/components/Web.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [listItem, setListItem] = useState([]);
+  const [task, setTask] = useState("");
+ 
+
+  function handleChange(e) {
+    setTask(e.target.value);
+    }
+
+    function addItems () {
+      setListItem(prev => {
+        return [...prev, task];
+      })
+
+      setTask("");
+    };
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
+        <div className="form">
+          <input
+            type="text" name="item"
+            placeholder="Add task"
+            onChange={handleChange}
+            value={task} />
+          <button onClick={addItems}>
+            <span>Add</span>
+          </button>
+        </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {listItem.map(item => <li>{ item }</li> )}
         </ul>
       </div>
     </div>
